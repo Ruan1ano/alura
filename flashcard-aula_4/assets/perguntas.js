ce02cfcb63769ddf01612b1d1743108cc6a0b6b0
Ruan1ano <144146855+Ruan1ano@users.noreply.github.com>
2024-11-11 11:49:58 -0300

    Add files via upload

---
+++ flashcard-aula_4/assets/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    "Programaçao"
+    "O que e JavaScript"
+    "O JavaScripit e uma linguagem de programaçao"
+)
+
+CriaCartao(
+    "Geografia",
+    "Qual a Capital da França?"
+    "A Capital da França e Paris"
+)
+
+criaCartao(
+    "Programaçao"
+    "O que e uma funçao?"
+    "Uma funçao e umbloco de codigo que executa alguma tarefa"
+)
